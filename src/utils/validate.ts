e3f5b0dee3dbb21034a3c0ea35d4671f00a1a464
GuillermoCAB <guiga.benitez@gmail.com>
2025-07-12 18:07:19 -0300

    feat: add validate password util

---
+++ src/utils/validate.ts
@@ -0,0 +1,17 @@
+export const validatePassword = (password: string): void => {
+  if (password.length < 8) {
+    throw new Error("Password must be at least 8 characters long.");
+  }
+  if (!/[A-Z]/.test(password)) {
+    throw new Error("Password must contain at least one uppercase letter.");
+  }
+  if (!/[a-z]/.test(password)) {
+    throw new Error("Password must contain at least one lowercase letter.");
+  }
+  if (!/[0-9]/.test(password)) {
+    throw new Error("Password must contain at least one number.");
+  }
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    throw new Error("Password must contain at least one special character.");
+  }
+};
