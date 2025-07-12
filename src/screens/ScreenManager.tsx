aab26c9a46fbf8343723803abe0c86181aeb9b48
GuillermoCAB <guiga.benitez@gmail.com>
2025-07-12 17:54:21 -0300

    feat: update screen manager to show correct UI

--- src/screens/ScreenManager.tsx
+++ src/screens/ScreenManager.tsx
@@ -1,12 +1,48 @@
-import { useState } from "react";
 import { GlobalLoader } from "../components";
+import { useSelector } from "react-redux";
+import { useConnectToNetwork } from "../hooks";
+import { selectEncryptionKey, selectHasVault } from "../state/config/selectors";
+import { useCheckVault } from "../hooks/useCheckVault";
+import { selectIsConnected } from "../state/networks/selectors";
+import { useMemo } from "react";
 
 export function ScreenManager() {
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading: networkIsLoading } = useConnectToNetwork();
+  const { isLoading: vaultIsLoading } = useCheckVault();
+
+  const hasVault = useSelector(selectHasVault);
+  const hasPassword = !!useSelector(selectEncryptionKey);
+  const isConnected = useSelector(selectIsConnected);
+  const isLoading = networkIsLoading || vaultIsLoading;
+  const isNewUser = !hasPassword && !hasVault;
+
+  const ScreenToRender = useMemo(() => {
+    console.log("loggui ScreenManager rendering", {
+      hasVault,
+      hasPassword,
+      isConnected,
+      isNewUser,
+    });
+
+    if (!isConnected)
+      return (
+        <div className="p-4 w-full h-full">
+          Network not connected. Please select a network and try again.
+        </div>
+      );
+
+    if (isNewUser) return "NewPassword";
+
+    if (!hasVault) return "AddWallet";
+
+    if (!hasPassword) return "LoginForm";
+
+    return "Wallet";
+  }, [isConnected, isNewUser, hasPassword, hasVault]);
 
   if (isLoading) {
     return <GlobalLoader />;
   }
 
-  return <div className="w-full h-full flex flex-col">Hello</div>;
+  return <div className="w-full h-full flex flex-col">{ScreenToRender}</div>;
 }
