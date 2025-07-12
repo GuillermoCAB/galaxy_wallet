a8e36f26a0c8dc083c1bad9abaa910fbb16043f1
GuillermoCAB <guiga.benitez@gmail.com>
2025-07-12 16:01:44 -0300

    faet: initial clean up

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: "popup.html",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 });
